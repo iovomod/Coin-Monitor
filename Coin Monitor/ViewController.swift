cb9fd30fd7d162122d02de821280f78e6dba87ae
iovomod <iovomod@inbox.ru>
2017-07-09 22:43:18 +0300

    Add TableView and CollectionView

--- Coin Monitor/ViewController.swift
+++ Coin Monitor/ViewController.swift
@@ -8,22 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     let userServices = UserServices()
+    var coins: [String] = ["BTC", "ETH", "DASH", "ZEC"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        showUsers()
-        print("New Data")
-        userServices.downloadUsers {[weak self] in
-            self?.showUsers()
-        }
-
-        
+//        showUsers()
+//        print("New Data")
+//        userServices.downloadUsers {[weak self] in
+//            self?.showUsers()
+//        }
     }
-    
     
     func showUsers() {
         let companyes = userServices.getCompanyes()
@@ -33,6 +31,22 @@ class ViewController: UIViewController {
                 print("Company: \(company.name), UserName: \(user.surename)")
             }
         }
+    }
+    
+    //MARK: - UITableView
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = coins[indexPath.row]
+        return cell
     }
 }
 
