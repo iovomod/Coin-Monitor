18ab52fa32460b2cf3cb2c1880532235a444cab9
iovomod <macbookair@MacBook-Air-macbook.local>
2017-07-09 14:00:19 +0300

    Initial commit for Xcode

---
+++ Coin Monitor/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Coin Monitor
+//
+//  Created by iovomod on 05/07/2017.
+//  Copyright © 2017 Semerikov Sergey. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import Realm
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
---
+++ Pods/Target Support Files/Pods-Coin Monitor/Pods-Coin Monitor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coin_Monitor : NSObject
+@end
+@implementation PodsDummy_Pods_Coin_Monitor
+@end
